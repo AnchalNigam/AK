b26cce6080445bac1a9358dd8ae5877ef29fd61c
Anchal Nigam <anchalnigamm@gmail.com>
2019-01-28 18:56:41 +0530

    #10-some minor changes

--- src/components/shared/list/userList.presentation.js
+++ src/components/shared/list/userList.presentation.js
@@ -3,19 +3,21 @@ import '../chatlist/chatlist.presentation.css'
 import './userList.presentation.css'
 import getFirtCharAsImage from './../firstchardesign/firstCharDesign.presentation';
 import firstCharUpperCase from './../../../utils/firstCharCapital';
+import convertToDate from './../../../utils/timeToDateConversion';
 
 export const UserList = ({click,userDetail}) => (
          <div onClick={click} className="card my-card" id="card-design">
            <div className="card-body p-2">
                <div className="row ">
-                   <div className="col-lg-3 col-3 col-sm-2  col-md-3">
+                   <div className="col-lg-3 col-3 col-sm-2 center_div col-md-3">
                      {userDetail.profilePicUrl==null?getFirtCharAsImage(userDetail.firstName):<img src={userDetail.profilePicUrl} alt="user_photo" className="img-adjust img-fluid rounded-circle"/>}
                    </div>
                    <div className="col-lg-9 col-9 col-sm-10 col-md-9">
-                      <h5 className='d-block font-adjust-heading heading_color'>{firstCharUpperCase(userDetail.firstName)}</h5>
-                      <h5 className="font-adjust-heading subheading_color"><small>University Of Monash</small></h5>
-                      <h5 className="font-adjust-heading subheading_color"><small>Bachelor Of Technology</small></h5>
-                      <i className="grey_color"><h5 className="font-adjust-heading subheading_color"><small>Last Active:12:38 AM</small></h5></i>
+                      <h5 className='d-block font-adjust-heading heading_color'>{firstCharUpperCase(userDetail.firstName)} {userDetail.lastName!==null?firstCharUpperCase(userDetail.lastName):''}</h5>
+                      <h5 className="font-adjust-heading subheading_color"><small>{userDetail.university!=null?userDetail.university.name:''}</small></h5>
+                      <h5 className="font-adjust-heading subheading_color"><small>{userDetail.course!=null?userDetail.course.name:''}</small></h5>
+                      <h5 className="font-adjust-heading subheading_color"><small>{userDetail.country!=null?userDetail.country:''}</small></h5>
+                      <i className="grey_color"><h5 className="font-adjust-heading subheading_color"><small>Last Active:{convertToDate(userDetail.lastActive)}</small></h5></i>
                    </div>
                
                    {/* <div className="col-lg-3 col-3 col-sm-2 col-md-3">
--- src/utils/firstCharCapital.js
+++ src/utils/firstCharCapital.js
@@ -1,6 +1,6 @@
 //method to make first character in capital letter
 export function firstCharUpperCase(name){
-    name=name.replace(/ /g,'')
+    name=name.trim();
     return name.charAt(0).toUpperCase()+name.slice(1);
 }
 
