7c6297240189f1a2d804a43d82e5b21c200d259c
syashakash <akashshivram9@gmail.com>
2019-01-29 18:42:29 +0530

    #10 Add loader

---
+++ src/components/shared/loader/loader.presentation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loader.presentation.css';
+
+export const Loader = () => (
+    // <div className="container">
+    // <div className="row div_center">
+    //     <div className="col-12 ">
+              
+    //         </div>
+    //     </div>
+    // </div>
+    <div className="div-center">
+              <div className="lds-dual-ring"></div>
+
+    </div>
+)
