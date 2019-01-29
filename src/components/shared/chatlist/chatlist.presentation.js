8b449e051a4e92291c22a6b2c248ddbcb80f6184
syashakash <akashshivram9@gmail.com>
2019-01-29 18:35:01 +0530

    #10 Add label design strip to chatlist items specific to usertype

--- src/components/shared/chatlist/chatlist.presentation.js
+++ src/components/shared/chatlist/chatlist.presentation.js
@@ -3,17 +3,19 @@ import './chatlist.presentation.css'
 import convertToDate from './../../../utils/timeToDateConversion';
 import getFirtCharAsImage from './../firstchardesign/firstCharDesign.presentation';
 import firstCharUpperCase from './../../../utils/firstCharCapital';
+import getLastWord from './../../../utils/getlastword';
+import isMentor from './../../../utils/ismentor';
 
 export const ChatList = ({click,userDetail}) => (
    <div onClick={click} className="card my-card" id="card-design">
-      <div className="card-body p-2">
+      <div className={isMentor(getLastWord(userDetail.name))===true?"card-body p-2 border-left border-3 mentor_border" : "card-body p-2 border-left border-3 student_border"}>
          <div className="row ">
             <div className="col-lg-3 col-3 col-sm-2  col-md-3 div_center">
                {userDetail.imageUrl == null ? getFirtCharAsImage(userDetail.name) : <img src={userDetail.imageUrl} alt="user_photo" className="img-adjust img-fluid rounded-circle"/>}
             </div>
             <div className="col-lg-6 col-6 col-sm-8  col-md-6">
-               <span className="d-block font-adjust-heading">{firstCharUpperCase(userDetail.name)}<div className="circle bg bg-success rounded-circle"></div></span>
-               <span className="font-adjust-heading"><small>{userDetail.lastMessage==null? "" : userDetail.lastMessage.userDetail.lastMessage.messageText.slice(0,22)+"..."}</small></span>
+               <span className="d-block font-adjust-heading heading_color">{firstCharUpperCase(userDetail.name)}</span>
+               <span className="font-adjust-heading subheading_color"><small>{userDetail.lastMessage==null? "" : userDetail.lastMessage.messageText.slice(0,22)+"..."}</small></span>
             </div>
       
             <div className="col-lg-3 col-3 col-sm-2 col-md-3">
