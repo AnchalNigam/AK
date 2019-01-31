926f20ef1b3d49e2f7616faf066062cd942176b0
syashakash <akashshivram9@gmail.com>
2019-01-31 13:30:28 +0530

    #10 courtesy commit

--- src/config/development.js
+++ src/config/development.js
@@ -1,8 +1,8 @@
 let urls={
 }
 
-// urls.baseUrl="https://testapi.admitkard.com";
-urls.baseUrl="http://192.168.1.110:3001";
+urls.baseUrl="https://testapi.admitkard.com";
+// urls.baseUrl="http://192.168.1.110:3001";
 // urls.chatUrl="https://chat.admitkard.com";
 urls.chatUrl="http://localhost:3001";
 
